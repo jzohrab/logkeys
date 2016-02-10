01b2d9341d9ed24a1250a69694319cc17b92218d
Jeff Zohrab <jeff.zohrab@rakops.com>
2016-02-10 17:51:30 -0500

    WIP

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,18 @@
 #! /bin/sh
+set -e
 
 # Use this script to bootstrap your build AFTER checking it out from
 # source control. You should not have to use it for anything else.
 
 # Runs autoconf, autoheader, aclocal, automake, autopoint, libtoolize
-echo "Regenerating autotools files"
+echo
+echo "Regenerating autotools files ..."
 aclocal \
 && autoheader \
 && automake --add-missing \
 && autoconf
 
-echo "Now run ./configure, make, and make install."
+echo "... done.  Now please do the following:"
+echo
+echo "   cd build; ../configure; make; su; make install"
+echo
